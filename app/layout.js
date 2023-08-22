d089a4507bed3fe683824564445f7c606301cbaa
Umair <umairwaraich2019@gmail.com>
2023-08-22 11:18:46 +0500

    added contact form submission

--- app/layout.js
+++ app/layout.js
@@ -9,12 +9,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link
-          rel="icon"
-          href="/logo.png"
-          type="image/<generated>"
-          sizes="<generated>"
-        />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
       <body>{children}</body>
     </html>
